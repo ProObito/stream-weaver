50de8560f4c14708220d2c696e746927e19042a9
ProObito <ahmedumaid707@gmail.com>
2026-01-11 21:35:56 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,15 +7,22 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Models ensure karein
+// Models
 require('./models/Series');
 require('./models/Episode');
 
+// STATIC FILES CONFIG
+// Ye line check kar, 'public' folder ko serve karne ke liye
 app.use(express.static(path.join(__dirname, '../public')));
 
 // Routes
 const adminRoutes = require('./routes/admin.routes');
 app.use('/api/admin', adminRoutes);
+
+// Agar koi seedha /admin par jaye toh use index.html bhejo
+app.get('/admin', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/admin/index.html'));
+});
 
 app.get('/', (req, res) => {
     res.send('Stream Weaver Backend Running');
